4c4c67e900edb218559ce8f524afda8c2d9d5a3e
Denis Antonov <den69ant@yandex.ru>
2022-07-25 18:45:44 +0300

    test was updated

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_product_page_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
+    time.sleep(10)
     basket_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert basket_button != None, "Basket button was not found"

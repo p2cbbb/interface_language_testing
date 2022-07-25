ea647b0b87398a08349892580dc593e460811f3e
Denis Antonov <den69ant@yandex.ru>
2022-07-25 18:40:24 +0300

    test was updated

--- test_items.py
+++ test_items.py
@@ -3,17 +3,10 @@ from selenium.webdriver.common.by import By
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-# Add to basket button in different languages
-basket_buttons_lang = ['أضف الى سلة التسوق', 'Afegeix a la cistella', 'Vložit do košíku', 'Læg i kurv', 'In Warenkorb legen',
-                       'Add to basket', 'Προσθήκη στο καλάθι', 'Añadir al carrito', 'Lisää koriin', 'Ajouter au panier', 
-                       'Aggiungi al carrello', '장바구니 담', 'Voeg aan winkelmand toe', 'Dodaj do koszyka', 'Adicionar ao carrinho', 
-                       'Adicionar à cesta', 'Adauga in cos', 'Pridať do košíka', 'Добавить в корзину', 'Додати в кошик']
 
 
 def test_product_page_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(30)
-    # Find the button and check if it is in the list
     basket_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    basket_button_text = basket_button.text
-    assert basket_button_text in basket_buttons_lang, "Basket button was not found"
+    assert basket_button != None, "Basket button was not found"
